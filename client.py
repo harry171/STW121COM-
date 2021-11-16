5fa4e5c63bb453db4c01874975507ea0c436cec8
Hari Narayan Chaudhary <thisizhary27@gmail.com>
2021-11-16 23:50:19 +0545

    creating connection

--- client.py
+++ client.py
@@ -22,6 +22,48 @@ SMALL_FONT = ("Helvetica", 13)
 # SOCK_STREAM: we are using TCP packets for communication
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+def add_message(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + '\n')
+    message_box.config(state=tk.DISABLED)
+
+def connect():
+
+    # try except block
+    try:
+
+        # Connect to the server
+        client.connect((HOST, PORT))
+        print("Successfully connected to server")
+        add_message("[SERVER] Successfully connected to the server")
+    except:
+        messagebox.showerror("Unable to connect to server", f"Unable to connect to server {HOST} {PORT}")
+
+    username = username_textbox.get()
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        messagebox.showerror("Invalid username", "Username cannot be empty")
+
+    threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
+
+    username_textbox.config(state=tk.DISABLED)
+    username_button.config(state=tk.DISABLED)
+
+# it will send message to the all the clients
+def send_message():
+    message = message_textbox.get()
+    if message != '':
+        client.sendall(message.encode())
+        message_textbox.delete(0, len(message))
+    else:
+        messagebox.showerror("Empty message", "Message cannot be empty")
+
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Messenger Client App")
+root.resizable(False, False)
+
 
 
 # main function
