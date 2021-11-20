b98d90d2454efe467801f0000ef1ee67e467a121
Hari Narayan Chaudhary <thisizhary27@gmail.com>
2021-11-20 12:54:06 +0545

    Starting server code

---
+++ server.py
@@ -0,0 +1,18 @@
+# Import required modules
+import socket
+import threading
+
+HOST = '127.0.0.1'
+PORT = 1234 # we can use any port between 0 to 65535
+LISTENER_LIMIT = 5
+active_clients = [] # List of all currently connected users
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
