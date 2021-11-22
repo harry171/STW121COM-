09c10a09d8305b11acd5ee7fdb1bbc64110e0603
Hari Narayan Chaudhary <thisizhary27@gmail.com>
2021-11-22 19:55:22 +0545

    server listening and sending message to client

--- server.py
+++ server.py
@@ -7,7 +7,33 @@ PORT = 1234 # we can use any port between 0 to 65535
 LISTENER_LIMIT = 5
 active_clients = [] # List of all currently connected users
 
+# Function to listen for upcoming messages from a client
+def listen_for_messages(client, username):
 
+    while 1:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+
+        else:
+            print(f"The message send from client {username} is empty")
+
+
+# Function to send message to a single client
+def send_message_to_client(client, message):
+
+    client.sendall(message.encode())
+
+# Function to send any new message to all the clients that
+# are currently connected to this server
+def send_messages_to_all(message):
+
+    for user in active_clients:
+
+        send_message_to_client(user[1], message)
 
 
 
